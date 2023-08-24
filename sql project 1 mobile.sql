71953580f12cb2cf76b4dbb3a7d8726f04b188be
MohitPanchal2000 <142791370+MohitPanchal2000@users.noreply.github.com>
2023-08-25 00:21:19 +0530

    Add files via upload
    Project Title: Mobile Device Market Analysis using SQL and Power BI
    
    Description:
    In this project, I undertook a comprehensive data analysis and visualization initiative centered around the dynamic mobile device market. By leveraging my expertise in SQL and Power BI, I meticulously collected, analyzed, and visualized data pertaining to mobile devices, aiming to extract valuable insights to inform strategic decisions.
    
    Key Accomplishments:
    
    *Data Compilation and Cleansing: Compiled an extensive dataset encompassing diverse facets of the mobile industry, including specifications, pricing, user preferences, and market trends. Employed data cleansing techniques to ensure data accuracy and consistency, providing a solid foundation for meaningful analysis.
    
    *SQL Database Design and Management: Devised and implemented an optimized SQL database structure to efficiently house the complex mobile device dataset. Developed intricate queries to extract pertinent information for analysis, underscoring my adeptness in SQL querying.
    
    *Market Trend Identification: Conducted comprehensive analyses to discern prevailing trends within the mobile device sector. Uncovered patterns such as shifts in feature demand, brand preferences, and emerging market segments, contributing to informed decision-making.
    
    *Interactive Data Visualization: Utilized Power BI to transform raw data into dynamic, interactive dashboards. Employed a range of visualizations including line graphs, heat maps, and scatter plots to convey insights effectively to both technical and non-technical stakeholders.
    
    *Predictive Modeling (Optional, if applicable): Employed advanced analytics techniques like regression analysis to create predictive models that forecast market demand, pricing fluctuations, or technology adoption rates, enhancing the project's strategic value.
    
    *Insightful Reporting: Generated comprehensive reports summarizing analyses, key insights, and actionable recommendations derived from the project. These reports served as vital resources for executive-level discussions and guided business strategies.
    
    *Collaborative Approach: Collaborated closely with cross-functional teams to understand their specific informational needs and incorporated their feedback throughout the analysis and visualization process, showcasing my adept teamwork and communication abilities.
    
    Technologies and Skills Demonstrated:
    
    -SQL (Structured Query Language)
    -Power BI (Business Intelligence)
    -Data Compilation and Cleansing
    -Data Analysis and Visualization
    -Query Optimization
    -Data Modeling (Optional)
    -Predictive Modeling (Optional)
    -Interactive Dashboard Design
    -Report Generation
    This project underscores my expertise in SQL and Power BI, exemplifying my capacity to navigate intricate datasets, extract actionable insights, and effectively communicate findings. It underscores my commitment to employing data-driven methodologies to guide strategic decisions in a competitive mobile device landscape.

---
+++ sql project 1 mobile.sql
@@ -0,0 +1,57 @@
+select * from mobile
+
+	-- check mobile features and price list--
+select Phone_name,price from mobile
+
+	-- find out the price of 5 most expensive phones--
+	select * from mobile 
+	order by price desc 
+	limit 5
+
+	-- find out the price of 5 most cheapest phones--
+	select * from mobile 
+	order by price asc 
+	limit 5 
+
+	-- list of top 5 samsung phones with price and all features--
+	select * from mobile
+	where brands = "samsung"
+	order by price desc
+	limit 5
+
+	-- must have android phone list then top 5 high price android phones--
+	select * from mobile
+	where Operating_System_Type = "android"
+	order by price desc 
+	limit 5
+
+	-- must have android phone list then top 5 lower price android phones--
+	select * from mobile
+	where Operating_System_Type = "android"
+	order by price asc
+	limit 5
+
+	-- must have ios phone list then top 5 high price ios phones--
+	select * from mobile 
+	where Operating_System_Type = "ios"
+	order by price desc 
+	limit 5 
+
+	-- must have ios phone list then top 5 lower price ios phones--
+	select * from mobile 
+	where Operating_System_Type = "ios"
+	order by price asc
+	limit 5
+
+	-- write a query which phone support 5g and also top 5 phone with 5g support--
+	select * from mobile 
+	where 5G_Availability = "yes" 
+	order by price desc 
+	limit 5 
+
+	-- total price of all mobile is to be found with brand name --
+	select brands,sum(price), from mobile 
+	group by Brands
+
+
+
